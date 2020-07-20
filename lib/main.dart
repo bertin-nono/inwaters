ebfa1188ed18d21d1ffe21383e4466ac79bb70ab
Bertin Nono <bnono@servicesource.com>
2020-07-20 06:14:27 -0600

    title in Main updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'inwaters',
+      title: 'Internation Network of Water Systems',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
